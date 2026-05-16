ca379575b8b52d1b8800caf99e664d8269e72082
Puras Handharmahua <puras.handharmahua@mekari.com>
2026-05-16 13:46:57 +0700

    Initial Commit

---
+++ Melodify/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Melodify
+//
+//  Created by puras.handharmahua@mekari.com on 16/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Melodify/MelodifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MelodifyApp.swift
+//  Melodify
+//
+//  Created by puras.handharmahua@mekari.com on 16/05/26.
+//
+
+import SwiftUI
+
+@main
+struct MelodifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
